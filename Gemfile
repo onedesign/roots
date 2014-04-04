23860180d1fc2b30ee96232f946fb07f892c1d63
Brian Hanson <brian@brianhanson.net>
2014-04-04 14:14:05 -0500

    add our grunt processess and scratch

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+gem "sass", "~>3.3.0"
+gem 'compass', '~> 1.0.0.alpha.19'
+gem "singularitygs", "~>1.1.2"
+gem 'toolkit', '~>2.0'
+gem 'modular-scale', '~> 2.0.4'
+gem 'sass-globbing', '~> 1.1.0'
---
+++ Gruntfile.js
@@ -0,0 +1,169 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    // Task configuration.
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: [
+          'assets/bower_components/jquery/dist/jquery.min.js', // take this out for WP dev
+          'assets/plugins/*.js',
+          'assets/main.js'
+        ],
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'dist/<%= pkg.name %>.min.js'
+      },
+      modernizr: {
+        src: 'assets/bower_components/modernizr/modernizr.js',
+        dest: 'dist/modernizr.min.js'
+      }
+    },
+    compass: {
+      dist: {
+        options:{
+          outputStyle: 'compressed',
+          config: 'config.rb',
+        },
+      },
+      patterns: {
+        options: {
+          cssDir: 'patterns/public/styleguide/css',
+          outputStyle: 'nested'
+        }
+      },
+    },
+    autoprefixer: {
+      options:{
+        browsers: ['last 2 version', 'ie 8', '> 5%']
+      },
+      dist: {
+        src: 'dist/<%= pkg.name %>.css',
+        dest: 'dist/<%= pkg.name %>.css'
+      },
+    },
+    coffee: {
+      dist: {
+        expand: true,
+        flatten: true,
+        cwd: 'src/scripts/',
+        src: ['*.coffee'],
+        dest: 'assets/scripts/compiled',
+        ext: '.js'
+      }
+    },
+    coffee_jshint: {
+      source: {
+        src: ['assets/scripts/**/*.coffee']
+      },
+    },
+    pixrem: {
+      options: {
+        rootvalue: '100%' // Set this to be the same as your stylesheet
+      },
+      dist: {
+        src: 'dist/<%= pkg.name %>.css',
+        dest: 'dist/<%= pkg.name %>.css'
+      }
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {
+          jQuery: true
+        }
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      }
+    },
+    imagemin: {
+      dist:{
+        files: [{
+          expand: true,
+          cwd: 'assets/img',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'dist/img/'
+        }]
+      }
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    watch: {
+
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile', 'buildscripts', 'buildstyles','optimizeimages']
+      },
+      scripts:{
+        files: ['assets/scripts/**/*.js', 'assets/scripts/**/*.coffee'],
+        tasks: ['buildscripts'],
+      },
+      styles:{
+        files: ['assets/styles/**/*.scss'],
+        tasks: ['buildstyles'],
+      },
+      reload:{
+        files: ['dist/**/*.css', 'dist/**/*.js', '**/*.html'],
+        options:{
+          livereload: true,
+        }
+      },
+      images:{
+        files: ['assets/img/**/*.{png, jpg, gif}'],
+        tasks: ['optimizeimages']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-coffee-jshint');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  // grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-pixrem');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+
+  // Default task.
+  grunt.registerTask('default', ['buildscripts', 'buildstyles', 'optimizeimages']);
+  grunt.registerTask('buildscripts', ['coffee_jshint', 'coffee', 'concat', 'uglify']);
+  grunt.registerTask('buildstyles', ['compass', 'autoprefixer','pixrem']);
+  grunt.registerTask('optimizeimages', ['imagemin']);
+};
